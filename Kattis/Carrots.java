189fdd2eb05471077b017bae58e54a3dd675c0a1
Thales Ferreira <thales.ferreira@ryerson.ca>
2016-02-29 14:54:23 -0500

    Create Carrots.java

---
+++ Kattis/Carrots.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Carrots {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        int carrots = in.nextInt();
+        in.close();
+        System.out.print(carrots);
+    }
+}
