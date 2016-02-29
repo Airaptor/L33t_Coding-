1e80556cc8d258453816fd5608c1c34f7620cfc8
Thales Ferreira <thales.ferreira@ryerson.ca>
2016-02-29 12:38:41 -0500

    Create DiagonalDifference.java
    Given a square matrix of size [N×N] [N×N], calculate the absolute difference between the sums of its diagonals.

---
+++ hacker_Rank/DiagonalDifference.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DiagonalDifference {
+    public static int diagonal, anti_diagonal;
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[][] = new int[n][n];
+        
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+                
+                if(a_i==a_j)
+                {
+                    diagonal += a[a_i][a_j];
+                }
+                if(a_i == n-1-a_j) 
+                {
+                    anti_diagonal += a[a_i][a_j];
+               }
+            }
+        }
+        System.out.println(Math.abs(anti_diagonal - diagonal) );
+    }
+}
